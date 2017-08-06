49b2c58e782768138473e498a090ec37cbdba10b
srikigiri <30624350+srikigiri@users.noreply.github.com>
2017-08-06 21:14:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ function createTemplate (data){
     var heading= data.heading;
     var date= data.date;
     var content = data.content;
-    var htmltemplate = `
+    var htmlTemplate = `
     <html>
         <head>
             <link href="/ui/style.css" rel="stylesheet" />
